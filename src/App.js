4bb8fc5f48c91abdd9be67d48062eef3e609a38d
willyfontenelle <willyfontenelle@gmail.com>
2022-08-19 16:19:30 -0300

    fim aula 01 modulo 04

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='grid container grid-cols-12 gap-x-1 md:gap-x-2 lg:gap-x-3'>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+        <div>abc</div>
+      </div>
     </div>
   );
 }
